3a60c94a493f9633243d6621cf55e3a85e0bd0dc
Karthu097 <chandrapatiKarthikeya2006@gmail.com>
2026-06-07 20:11:11 +0530

    SetMatrixZeros

--- src/main/java/TakeUForward/SetMatrixZeroes.java
+++ src/main/java/TakeUForward/SetMatrixZeroes.java
@@ -1,4 +1,38 @@
 package TakeUForward;
 
 public class SetMatrixZeroes {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {0, 1, 2},
+                {1, 1, 1},
+                {1, 1, 0}
+
+        };
+        boolean[] zeroRows = new boolean[3];
+        boolean[] zeroCols = new boolean[3];
+
+        //step1 :finding zeros
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+             if (matrix[i][j] == 0) {
+                    zeroRows[i] = true;
+                    zeroCols[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (zeroRows[i] || zeroCols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
